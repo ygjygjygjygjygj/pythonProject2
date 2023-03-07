9c80fa6ef85700c360e95b042439c4987d6b2c1f
13989 <13989@ygj>
2023-03-07 10:02:20 +0800

    。。

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('zccssb')
+    print_hi('zccssb!!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
