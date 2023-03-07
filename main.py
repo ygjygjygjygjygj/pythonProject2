0b51f8f4cb956b7c95c500c8f4c39fe6c49c8d2c
13989 <13989@ygj>
2023-03-07 22:05:45 +0800

    time-solved

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 data = pd.read_csv('D:\\xzDate\\data\\.ipynb_checkpoints\\train-checkpoint.csv', dtype={48: str, 51: str, 52: str})
 data.head() # 查看前几行数据
 data.describe() # 查看数据的统计信息
 data.fillna(0, inplace=True)
 data.drop_duplicates(inplace=True)
 data = data.apply(lambda x: x * 2)
-data.to_csv('D:\\xzDate\\cleaned_data.csv', index=False)
+data.to_csv('D:\\xzDate\\data\\cleaned_data.csv', index=False)
